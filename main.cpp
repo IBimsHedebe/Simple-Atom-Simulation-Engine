129ef5ac480ee62e64c780f4edc331bc6cac2e4c
Ole Snyders <ole.snyders@windthorstgymnasium.de>
2026-09-18 17:51:14 +0200

    first gui

--- main.cpp
+++ main.cpp
@@ -4,17 +4,41 @@
 #include "raygui.h"
 
 int main() {
-    InitWindow(800, 600, "Raygui Beispiel");
+    InitWindow(1280, 720, "Simple Atom Simulation Engine");
     SetTargetFPS(60);
 
+    bool dropdownEditMode = false;
+    int dropdownActive = 0;
+
+
+
     while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_F11)) ToggleFullscreen();
+
+        int currentWidth = GetScreenWidth();
+        
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // Button zeichnet sich selbst und gibt true zurück, wenn geklickt
-        if (GuiButton((Rectangle){ 350, 250, 100, 40 }, "Klick mich!")) {
-            // Aktion bei Klick
+        DrawRectangle(0,0, currentWidth, 40, LIGHTGRAY);
+        DrawLine(0, 40, currentWidth, 40, GRAY);
+
+        // Button 1 Dateien importieren
+        if (GuiButton((Rectangle){10, 5, 80, 30}, "#01# Datei")) {
+            TraceLog(LOG_INFO, "Imporitieren");
         }
+
+        // Button 2 Speichern
+        if (GuiButton((Rectangle){100, 5, 100, 30}, "#02# Speichern")) {
+            TraceLog(LOG_INFO, "Exportieren");
+        }
+
+        // Button 3 Tools
+        if (GuiDropdownBox((Rectangle){ 210, 5, 140, 30 }, "Stift;Radiergummi;Auswählen;Zoom", &dropdownActive, dropdownEditMode)) {
+            dropdownEditMode = !dropdownEditMode;
+        }
+        
+
 
         EndDrawing();
     }
