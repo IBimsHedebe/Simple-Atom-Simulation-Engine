50f1f350c4d084c2dbdbac1a92c25e44b780a7bd
Ole Snyders <ole.snyders@windthorstgymnasium.de>
2026-09-18 18:06:48 +0200

    first shortcuts

--- main.cpp
+++ main.cpp
@@ -3,6 +3,53 @@
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
+typedef struct {
+    const char *shortcut;
+    const char *name;
+} Tool;
+
+Tool tools[] = {
+    { "H", "Wasserstoff" },
+    { "C", "Kohlenstoff" },
+    { "O", "Sauerstoff" },
+};
+#define TOOL_COUNT (sizeof(tools) / sizeof(tools[0]))
+
+char inputBuffer[3] = "";
+const char *activeTool = "Keines";
+
+void UpdateShortcutSystem(void) {
+    int key = GetCharPressed();
+    
+    while (key > 0) {
+        if ((key >= 'a' && key <= 'z') || (key >= 'A' && key <= 'Z')) {
+            if (key >= 'a') key -= 32; // In Großbuchstaben umwandeln
+
+            int len = strlen(inputBuffer);
+            if (len < 2) {
+                inputBuffer[len] = (char)key;
+                inputBuffer[len + 1] = '\0'; // String-Ende setzen
+            }
+        }
+        key = GetCharPressed();
+    }
+
+    bool matchFound = false;
+    for (int i = 0; i < TOOL_COUNT; i++) {
+        if (strcmp(inputBuffer, tools[i].shortcut) == 0) {
+            activeTool = tools[i].name;
+            matchFound = true;
+            inputBuffer[0] = '\0';
+            break;
+        }
+    }
+
+    if (!matchFound && strlen(inputBuffer) >= 2) {
+        inputBuffer[0] = '\0';
+    }
+}
+
+
 int main() {
     InitWindow(1280, 720, "Simple Atom Simulation Engine");
     SetTargetFPS(60);
@@ -15,7 +62,14 @@ int main() {
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_F11)) ToggleFullscreen();
 
+        UpdateShortcutSystem();
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            activeTool = "Keines"; 
+        }
+
         int currentWidth = GetScreenWidth();
+        int currentHeight = GetScreenHeight();
         
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -34,11 +88,16 @@ int main() {
         }
 
         // Button 3 Tools
-        if (GuiDropdownBox((Rectangle){ 210, 5, 140, 30 }, "Stift;Radiergummi;Auswählen;Zoom", &dropdownActive, dropdownEditMode)) {
+        if (GuiDropdownBox((Rectangle){ 210, 5, 140, 30 }, "Wasserstoff;Kohlenstoff;Sauerstoff", &dropdownActive, dropdownEditMode)) {
             dropdownEditMode = !dropdownEditMode;
         }
-        
 
+        DrawText(TextFormat("Aktives Werkzeug: %s", activeTool), 50, 300, 24, MAROON);
+
+        const char *displayText = TextFormat("Eingabe: [%s]", inputBuffer);
+        int textWidth = MeasureText(displayText, 20);
+        
+        DrawText(displayText, currentWidth - textWidth - 20, currentHeight - 40, 20, BLUE);
 
         EndDrawing();
     }
